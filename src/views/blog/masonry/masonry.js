650b388ce1338280d471b91306fd1e08428cfee2
WindyWin <75520384+WindyWin@users.noreply.github.com>
2021-12-09 13:26:32 +0700

    .

--- src/views/blog/masonry/masonry.js
+++ src/views/blog/masonry/masonry.js
@@ -7,18 +7,39 @@ const Masonry = () => {
   return (
   <div className="masonry">
       <Baner1 banData={banData[0]}/>
-      <div className="items">
-        <div className="container">
-            <BlogItem blog={blogData[0]}/>
-            <BlogItem blog={blogData[1]}/>
-            <BlogItem blog={blogData[2]}/>
-            <PostText postText={postTextData[0]}/>
-            <BlogItem blog={blogData[3]}/>
-            <BlogItem blog={blogData[4]}/>
-            <BlogItem blog={blogData[5]}/>
-            <PostText postText={postTextData[1]}/>
-            <BlogItem blog={blogData[6]}/>
-        </div>
+      <div className="container">
+            <div className="item-1">
+              <BlogItem blog={blogData[0]}/>
+            </div>
+            <div className="item-2">
+              <BlogItem blog={blogData[1]}/>
+            </div>
+            <div className="item-3">
+              <BlogItem blog={blogData[2]}/>
+            </div>
+            <div className="item-4">
+              <PostText postText={postTextData[0]}/>
+            </div>
+            <div className="item-5">
+              <BlogItem blog={blogData[3]}/>
+            </div>
+            <div className="item-6">
+              <BlogItem blog={blogData[4]}/>
+            </div>
+            <div className="item-7">
+              <BlogItem blog={blogData[5]}/>
+            </div>
+            <div className="item-8">
+              <PostText postText={postTextData[1]}/>
+            </div>
+            <div className="item-9">
+              <BlogItem blog={blogData[6]}/>
+            </div>
+      </div>
+      <div className="btn">
+        <button>
+          LOAD MORE
+        </button>
       </div>
   </div>)
 };
